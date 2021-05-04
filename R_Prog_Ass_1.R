48c2af0ffdcbd604a595e435145f52f96c7f608c
BBhagria <83653815+BBhagria@users.noreply.github.com>
2021-05-04 19:40:10 +0530

    Update and rename R_Prog_Ass_1 to R_Prog_Ass_1.R

--- R_Prog_Ass_1.R
+++ R_Prog_Ass_1.R
@@ -1,5 +1,10 @@
 #makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
+#' Util function that set the matrix and the inverse in an environment
+#' @param x an invertible matrix
+#' examples
+#' x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
+#' x$set(matrix(rnorm(16), 4, 4))
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y){
@@ -13,6 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 #cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+#' Compute and cache the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
